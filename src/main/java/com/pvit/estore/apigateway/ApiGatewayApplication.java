cdae50fb0a0a07217669cca06ebce2035e818ef7
Paulo Viana <pauloc.fviana@hotmail.com>
2024-12-05 10:35:16 +0000

    config fixed

--- src/main/java/com/pvit/estore/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/pvit/estore/apigateway/ApiGatewayApplication.java
@@ -3,13 +3,22 @@ package com.pvit.estore.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@RequestMapping
+	@GetMapping
+	public void helloWorld() {
+		System.out.println("hello world!");
+	}
 }
